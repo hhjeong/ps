247340661d6b54b7e56dbfde32ec52d384216d79
Hyun-hwan Jeong <libe@ajou.ac.kr>
2012-03-10 09:57:30 +0900

    competition doesn't start yet

---
+++ GCJK_2012/R1B/D.cc
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef pair<int,int> ii;
+typedef long long ll;
+
+int main() {
+    clock_t start_time = clock();
+    freopen("D.in","r",stdin);
+
+    int ncase;
+    scanf("%d", &ncase); 
+
+    for( int caseno = 1 ; caseno <= ncase ; ++caseno ) {
+        printf("Case #%d: ", caseno);
+		fprintf(stderr,"%d/%d\n", caseno, ncase);
+        printf("\n");
+    }
+    fprintf(stderr,"Elapsed time : %.3fsec\n", (double)(clock()-start_time)/CLOCKS_PER_SEC);    
+}
